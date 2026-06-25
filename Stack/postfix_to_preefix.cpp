096a70f70bf40dddfc10f21b207439957b07dc86
Harshit Vaishnav <vaishnavharshit01@gmail.com>
2026-06-25 22:48:18 +0530

    postfix to preefix

---
+++ Stack/postfix_to_preefix.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string postToPre(string s)
+    {
+        int n = s.size();
+        stack<string> st;
+        for (int i = 0; i < n; i++)
+        {
+            char ch = s[i];
+            if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') ||
+                (ch >= '0' && ch <= '9'))
+            {
+                {
+                    st.push(string(1, ch));
+                }
+            }
+
+            else
+            {
+                string t1 = st.top();
+                st.pop();
+                string t2 = st.top();
+                st.pop();
+                string ans = ch + t2 + t1;
+                st.push(ans);
+            }
+        }
+        return st.top();
+    }
+};
